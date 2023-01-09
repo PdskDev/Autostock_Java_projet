4cc2aad962f7c92b7995bdedf8ee59f71a718f0b
PdskDev <prestadesk@gmail.com>
2023-01-09 23:55:15 +0100

    IndexController - index.html - Modify User class

--- Autostock/src/main/java/fr/eni/autostock/bo/Car.java
+++ Autostock/src/main/java/fr/eni/autostock/bo/Car.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -30,6 +31,7 @@ public class Car {
 	@NotNull
 	private String energy;
 	
+	@OneToMany
 	private List<Option> options;
 	
 	public Car() {
--- Autostock/src/main/java/fr/eni/autostock/bo/User.java
+++ Autostock/src/main/java/fr/eni/autostock/bo/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -58,6 +59,7 @@ public class User {
 	@NotNull
 	private String password;
 	
+	@OneToMany
 	private List<Caddie> listCaddies;
 	
 	
